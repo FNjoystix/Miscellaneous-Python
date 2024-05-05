085db17909c25d2449c4d8013525c649619ab274
FNjoystix <154027776+FNjoystix@users.noreply.github.com>
2024-05-05 19:35:32 +0000

    made a rng guessing game

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 for i in range(50):
-    print("\n")
-x = 0
-if x == 0:
-    y = int(input("Input a number: "))
-    print(str(y**2) + " <-- Here is the square of your input.")
+    print("\n")     
+
+import random
+r = random.randint(1,100)
+g = 0
+while r != g:
+    g = input("Guess: ")
+    if (int(g) > int(r)):
+        print("   Lower!")
+    elif(int(g) < int(r)):
+        print("   Higher!")
+    else:
+        print("Good job! That was the number!")
+        break
