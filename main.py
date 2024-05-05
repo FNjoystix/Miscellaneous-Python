439fb706cd0b56c32c54d678b081c578517beb91
FNjoystix <154027776+FNjoystix@users.noreply.github.com>
2024-05-05 00:30:36 +0000

    pretty much made the prgm

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello, World!")
-print("Goodbye, World!")
+for i in range(50):
+    print("\n")
+x = 0
+if x == 0:
+    y = int(input("Input a number: "))
+    print(str(y**2) + " <-- Here is the square of your input.")
