473a8ad6fd903b8e3297c159a01e2c1cb694ac10
FNjoystix <154027776+FNjoystix@users.noreply.github.com>
2024-05-11 08:41:00 -0400

    Update fishsim.py
    Added comments and more instructions

--- fishsim.py
+++ fishsim.py
@@ -2,6 +2,8 @@
 # - Make the fish reproduce 1 for every 2
 # - Make fish die occasionally to put some weight on round 1
 
+import random
+
 for i in range(50):
     print("\n")
 
@@ -9,19 +11,26 @@ fish_amount = 10
 money = 0
 rnd1 = 0 #These vars are called so that the variable is defined just in case the IF statement below is run improperly.
 rnd2 = 0
-print("Each fish is worth 1$ in the first round and 3$ in the second. There are two rounds per year.")
-print("8$ at the end of each year will be extracted for living expenses. If you cannot pay that, you lose.")
+
+print("Hello, and welcome to Fish Simulator!")
+print("- Each fish is worth 1$ in the first round and 3$ in the second. There are two rounds per year.")
+print("- 8$ at the end of each year will be extracted for living expenses. If get below 0$, you lose!")
+print("- In between rounds 1 and 2 there is a chance that some of your fish will die.")
+print("- If you try to take more fish than you have, the round will be skipped. Don't do it!")
 
 while money > -1:
     print("\n")
     print("You have " + str(money) + "$ and there are " + str(fish_amount) + " fish in your lake.")
+# Round 1
     take = input("How many fish would you like to take in the first round? ")
     if int(take) > fish_amount:
         print("You do not have that many fish.")
     else:
         print("   You take " + str(take) + " fish in round 1.")
         rnd1 = take
-        fish_amount = int(fish_amount) - int(rnd1)
+    fish_amount = int(fish_amount) - int(rnd1)
+    
+# Round 2
     take = input("How many fish would you like to take in the second round? ")
     if int(take) > fish_amount:
         print("You do not have that many fish.")
@@ -31,6 +40,9 @@ while money > -1:
         fish_amount = int(fish_amount) - int(rnd2)
     profit = (int(rnd1) + 3*int(rnd2))
     money = (int(money) + int(profit))
+
+# Buy Phase
+    print("You have " + str(money) + "$ and there are " + str(fish_amount) + " fish in your lake.")
     bought = input("How many fish would you like to buy? (1$ each) ")
     money = (int(money) - int(bought))
     fish_amount = int(fish_amount) + int(bought)
