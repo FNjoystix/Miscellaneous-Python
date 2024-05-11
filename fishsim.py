635577b75d35b42438d4ffeeaec9aa389b24b3cb
FNjoystix <154027776+FNjoystix@users.noreply.github.com>
2024-05-11 19:18:25 +0000

    reproduction and fish death

--- fishsim.py
+++ fishsim.py
@@ -1,8 +1,8 @@
 #To do:
-# - Make the fish reproduce 1 for every 2
-# - Make fish die occasionally to put some weight on round 1
+# Nothing currently
 
 import random
+import math
 
 for i in range(50):
     print("\n")
@@ -11,8 +11,9 @@ fish_amount = 10
 money = 0
 rnd1 = 0 #These vars are called so that the variable is defined just in case the IF statement below is run improperly.
 rnd2 = 0
+num_killed = 0
 
-print("Hello, and welcome to Fish Simulator!")
+print("Hello, and welcome to Fish Simulator v1.2!")
 print("- Each fish is worth 1$ in the first round and 3$ in the second. There are two rounds per year.")
 print("- 8$ at the end of each year will be extracted for living expenses. If get below 0$, you lose!")
 print("- In between rounds 1 and 2 there is a chance that some of your fish will die.")
@@ -29,7 +30,16 @@ while money > -1:
         print("   You take " + str(take) + " fish in round 1.")
         rnd1 = take
     fish_amount = int(fish_amount) - int(rnd1)
-    
+
+# Kill Phase
+    if fish_amount > 2:
+        prekill_amount = fish_amount - 2
+        for i in range(prekill_amount):
+            if random.randint(1,4) == 1:
+                num_killed = num_killed + 1
+                fish_amount = fish_amount - 1
+    print(str(num_killed) + " fish died.")
+        
 # Round 2
     take = input("How many fish would you like to take in the second round? ")
     if int(take) > fish_amount:
@@ -40,6 +50,11 @@ while money > -1:
         fish_amount = int(fish_amount) - int(rnd2)
     profit = (int(rnd1) + 3*int(rnd2))
     money = (int(money) + int(profit))
+
+# Reproduction Phase
+    num_born = math.floor(fish_amount / 2)
+    fish_amount = fish_amount + num_born
+    print(str(num_born) + " fish were born.")
 
 # Buy Phase
     print("You have " + str(money) + "$ and there are " + str(fish_amount) + " fish in your lake.")
