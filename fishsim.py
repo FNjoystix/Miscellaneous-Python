af49597caebb8d7fe60333b7aa9be05ccd83294d
elijahk2 <154027776+elijahk2@users.noreply.github.com>
2024-05-11 21:06:56 -0400

    Update fishsim.py
    Broke reproduction and fixed some other issues

--- fishsim.py
+++ fishsim.py
@@ -1,5 +1,6 @@
 #To do:
-# Nothing currently
+# Make the words "are" and "is" change depending on plurality
+# Fix reproduction
 
 import random
 import math
@@ -9,13 +10,12 @@ for i in range(50):
 
 fish_amount = 10
 money = 0
-rnd1 = 0 #These vars are called so that the variable is defined so you don't get "variable not defined" errors below."
+rnd1 = 0 #These vars are called so that the variable is defined just in case the IF statement below is run improperly.
 rnd2 = 0
-num_killed = 0
 
 print("Hello, and welcome to Fish Simulator v1.2!")
 print("- Each fish is worth 1$ in the first round and 3$ in the second. There are two rounds per year.")
-print("- 8$ at the end of each year will be extracted for living expenses. If get below 0$, you lose!")
+print("- 8$ at the end of each year will be extracted for living expenses. If you go below 0$, you lose!")
 print("- In between rounds 1 and 2 there is a chance that some of your fish will die.")
 print("- If you try to take more fish than you have, the round will be skipped. Don't do it!")
 
@@ -25,20 +25,20 @@ while money > -1:
 # Round 1
     take = input("How many fish would you like to take in the first round? ")
     if int(take) > fish_amount:
-        print("You do not have enough fish. Round skipped.")
+        print("You do not have that many fish.")
     else:
         print("   You take " + str(take) + " fish in round 1.")
         rnd1 = take
     fish_amount = int(fish_amount) - int(rnd1)
 
 # Kill Phase
-    if fish_amount > 2:
-        prekill_amount = fish_amount - 2
-        for i in range(prekill_amount):
-            if random.randint(1,4) == 1:
-                num_killed = num_killed + 1
-                fish_amount = fish_amount - 1
-    print(str(num_killed) + " fish died.")
+    prekill_amount = fish_amount
+    num_killed = 0
+    for i in range(prekill_amount):
+        if random.randint(1,4) == 1:
+            num_killed = num_killed + 1
+            fish_amount = fish_amount - 1
+    print("   " + str(num_killed) + " fish died. You have " + str(prekill_amount - num_killed) + " fish left.")
         
 # Round 2
     take = input("How many fish would you like to take in the second round? ")
@@ -52,9 +52,9 @@ while money > -1:
     money = (int(money) + int(profit))
 
 # Reproduction Phase
-    num_born = math.floor(fish_amount / 2)
-    fish_amount = fish_amount + num_born
-    print(str(num_born) + " fish were born.")
+    # num_born = math.floor(fish_amount / 2)
+    # fish_amount = fish_amount + num_born
+    # print("   " + str(num_born) + " fish were born.")
 
 # Buy Phase
     print("You have " + str(money) + "$ and there are " + str(fish_amount) + " fish in your lake.")
@@ -62,4 +62,5 @@ while money > -1:
     money = (int(money) - int(bought))
     fish_amount = int(fish_amount) + int(bought)
     money = int(money) - 8
+
 print("YOU HAVE RUN OUT OF MONEY. YOU LOSE!")
